85f9ecd13635ba23a4b545089ea44465af398df8
emchan@chapman.edu <ekc8888@gmail.com>
2018-11-28 12:19:55 -0800

    finish cars.sh

--- cars.sh
+++ cars.sh
@@ -2,26 +2,32 @@
 # cars.sh
 # Emily Chan
 
-echo "Old Cars Inventory Program"
-echo "1. Add a car"
-echo "2. List the cars in the inventory file"
-echo "3. Quit the program"
-read choice
-while [ "$choice" -ne  "3" ]
+while true
 do
-	case "$choice" in
-	"1") echo "Enter the year of the car:";;
-	read year
-	echo "Enter the make of the car:";;
-	read make
-	echo "Enter the model of the car";;
-	read model;
-	new_car = year+":"+make+":"+model
-	echo new_car >> My_old_cars
-	"2")
-	while read contents; do
-		echo $contents
-	done	
-	"3") break;;
-	esac
+	echo "Old Cars Inventory Program"
+	echo "1. Add a car"
+	echo "2. List the cars in the inventory file"
+	echo "3. Quit the program"
+	read choice
+
+		case "$choice" in
+			"1") echo "Enter the year of the car:"
+			read year
+			echo "Enter the make of the car:"
+			read make
+			echo "Enter the model of the car:"
+			read model;
+			newCar="$year":"$make":"$model"
+			echo $newCar >> My_old_cars.txt
+			echo "The car was added to inventory";;
+			"2") echo "Cars in inventory:" 
+			while read car
+			do
+			sort My_old_cars.txt
+			break
+			done < My_old_cars.txt;;
+                        "3") echo "Goodbye"
+                        break;;
+			
+		esac
 done
